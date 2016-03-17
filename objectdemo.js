a132c711914df47fdb9b93474b9d0e461e64c968
Ashu Lekhi <ashu.lekhi@tothenew.com>
2016-03-17 11:54:55 +0530

    secondcommit

---
+++ objectdemo.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ashu on 17/3/16.
+ */
+
+
+var person = 'ashu';
+var anotherperson = person;
+
+var person = 'kuldeep';
+
+console.log(person);
+console.log(anotherperson);
+
+var person =  anotherperson;
+
+console.log(person);
+
+
