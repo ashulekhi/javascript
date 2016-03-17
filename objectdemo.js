91a855633680371bb622a6ad9d0780710d0b1c30
Ashu Lekhi <ashu.lekhi@tothenew.com>
2016-03-17 12:27:25 +0530

    third commit

--- objectdemo.js
+++ objectdemo.js
@@ -3,7 +3,7 @@
  */
 
 
-var person = 'ashu';
+/*var person = 'ashu';
 var anotherperson = person;
 
 var person = 'kuldeep';
@@ -13,6 +13,29 @@ console.log(anotherperson);
 
 var person =  anotherperson;
 
-console.log(person);
+console.log(person);*/
+
+
+var person  = {name:'ashu'};
+
+var anotherperson = person;
+
+person.name='lekhi';
+
+console.log(person.name)
+console.log(person)
+console.log(anotherperson.name)
+console.log(anotherperson)
+
+var obj = {
+  name :'ashu'
+
+}
+obj.size = 5;
+console.log(obj.name);
+console.log(obj.propertyIsEnumerable('size'));
+
+
+
 
 
