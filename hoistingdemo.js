19eefaea4cb0e6d181f5b0e1b613b402c2a07e13
Ashu Lekhi <ashu.lekhi@tothenew.com>
2016-03-18 11:25:06 +0530

    hoisting commited

--- hoistingdemo.js
+++ hoistingdemo.js
@@ -25,3 +25,32 @@ console.log(name)
 var name = 'kuldeep'
 
 console.log(name)
+
+
+var name = 'ashu'
+{
+
+  var name = 'tanu'
+}
+
+console.log(name)
+
+
+console.log(i)
+
+for (i = 0 ; i<10 ; i++)
+{
+  console.log(i)
+}
+function a(){
+  var i = 15;
+  console.log('inside function' , i)
+}
+
+a();
+console.log(i)
+
+var i =20;
+console.log(i)
+
+console.log(i)
