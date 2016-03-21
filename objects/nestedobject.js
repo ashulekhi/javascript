1484a897d31cbab5b129370c87ad84606ffab1f5
Ashu Lekhi <ashu.lekhi@tothenew.com>
2016-03-21 12:33:01 +0530

    modifications in ibjects

--- objects/nestedobject.js
+++ objects/nestedobject.js
@@ -16,4 +16,18 @@ var car1 = new mycar('honda' ,2000,owner1 )
 
 console.log(car1)
 
+mycar.prototype.color = 'black';
+console.log('prinitng color ' , car1.color);
+
+console.log(Object.getOwnPropertyNames(mycar))
+
+
+for (var key in mycar){
+  if(mycar.hasOwnProperty(key))
+  console.log(key)
+}for (var key in car1){
+  if(car1.hasOwnProperty(key))
+  console.log(key)
+}
+
 
