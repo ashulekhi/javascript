82382ce671b20e4f311906ad5c608085dfc78f03
Ashu Lekhi <ashu.lekhi@tothenew.com>
2016-03-21 11:48:05 +0530

    objects

---
+++ objects/object1.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ashu on 21/3/16.
+ */
+
+
+var myobj = new Object();
+myobj.name = 'ashu'
+myobj.age ='24'
+myobj.job = 'software engineer'
+
+console.log(myobj);
+
+console.log(Object.getOwnPropertyNames(myobj));
+
+for (var i in myobj){
+  console.log('printing keys' , i + ' ' +  myobj[i] );
+}
+
+var obj = {
+
+  1: 'test',
+  key : 'value'
+}
+
+var keys = Object.keys(obj);
+
+console.log('keys of object are' , keys)
+
+console.log(obj["1"]);
